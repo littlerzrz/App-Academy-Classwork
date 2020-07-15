72602a39807f857117bf2017bfaa41e0a1c2a2fc
littlerzrz <renxinyi.design@gmail.com>
2020-07-15 15:04:13 +0800

    W6D3 Rails-router

---
+++ W6D3/rails-router/app/models/user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, presence:true, uniqueness:true
+
+    has_many :artworks, 
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :Artwork,
+        dependent: :destroy
+
+    has_many :views, 
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :ArtworkShare,
+        dependent: :destroy
+
+    has_many :shared_artworks, through: :views, source: :artwork
+
+
+end
+
+    
