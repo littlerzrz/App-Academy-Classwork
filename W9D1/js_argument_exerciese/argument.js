e56a6681efe3b955406449c34808f707ad6c04dc
littlerzrz <renxinyi.design@gmail.com>
2020-08-07 17:41:01 +0800

    W9D1 js arguments modifications

--- W9D1/js_argument_exerciese/argument.js
+++ W9D1/js_argument_exerciese/argument.js
@@ -53,6 +53,12 @@ Function.prototype.myBind = function (ctx, ...args) {
     };
 };
 
+// Function.prototype.myBind = function (ctx) {
+//     return () => {
+//         this.apply(ctx);
+//     };
+// };
+
 
 // Function.prototype.myBind = function (ctx, arg1, ...args) {
 //   return (args) => {
@@ -75,40 +81,83 @@ Function.prototype.myBind = function (ctx, ...bindArgs) {
 
 
 
-markov.says("meow", "Ned");
-markov.says.myBind(pavlov, "meow", "Kush")();
-markov.says.myBind(pavlov)("meow", "a tree");
-markov.says.myBind(pavlov, "meow")("Markov");
+// markov.says("meow", "Ned");
+// markov.says.myBind(pavlov, "meow", "Kush")();
+// markov.says.myBind(pavlov)("meow", "a tree");
+// markov.says.myBind(pavlov, "meow")("Markov");
+
+// const notMarkovSays = markov.says.myBind(pavlov);
+// notMarkovSays("meow", "me");
+
+
+curriedSum = (numArgs) => {
+     const nums = [];
+     const _curriedSum = (num) => {
+        nums.push(num)
+        if (nums.length === numArgs) {
+            let sumNums = nums.reduce(function(a, b) {
+                return a + b;
+            })
+            return sumNums;
+        } 
+        else {
+            return _curriedSum;
+        }
+     }
+    return _curriedSum;
+ }
+
+
+// const currySum = curriedSum(4);
+// console.log(currySum(5)(30)(20)(1)); // => 56
 
 
 
-const notMarkovSays = markov.says.myBind(pavlov);
-notMarkovSays("meow", "me");
+// Function.prototype.curry = function(numArgs) {
+//     const numbers = [];
+//     const _curry = (newNum) => {
+//         numbers.push(newNum)
+//         if (numbers.length === numArgs) {
+//             return this.apply(null,numbers)
+//         }
+//         else {
+//             return _curry
+//         }
+//     }
+//     return _curry
+// }
+
+
+Function.prototype.curry = function (numArgs) {
+    const numbers = [];
+    const _curry = (newNum) => {
+        numbers.push(newNum)
+        if (numbers.length === numArgs) {
+            return this(...numbers)
+        }
+        else {
+            return _curry
+        }
+    }
+    return _curry
+}
 
 
 
+function sumThree(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+sumThree(4, 20, 6); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+
+// or more briefly:
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Function.prototype.myBind = function (ctx) {
-//     return () => {
-//         this.apply(ctx);
-//     };
-// };
