5f8ec8d050d79d732213aa17347f8ea69852fbd8
littlerzrz <renxinyi.design@gmail.com>
2020-08-21 15:38:36 +0800

    W10D2 React Widgets updates

--- W10D2/widgets/frontend/tabs.jsx
+++ W10D2/widgets/frontend/tabs.jsx
@@ -1,5 +1,52 @@
 import React from "react";
 
 class Tabs extends React.Component {
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.children}</h1>
+      </div>
+    );
+  }
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selectedPane: 0,
+//     };
+//     this.selectTab = this.selectTab.bind(this);
+//   }
+
+//   selectTab(num) {
+//     this.setState({ selectedPane: num });
+//   }
+
+//   render() {
+//     const pane = this.props.panes[this.state.selectedPane];
+
+//     return (
+//       <div>
+//         <h1>Tabs</h1>
+//         <div className="tabs">
+//           <Headers
+//             selectedPane={this.state.selectedPane}
+//             onTabChosen={this.selectTab}
+//             panes={this.props.panes}
+//           ></Headers>
+//           <div className="tab-content">
+//             <article>{pane.content}</article>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
 }
+
+export default Tabs;
--- W10D2/widgets/frontend/widgets.jsx
+++ W10D2/widgets/frontend/widgets.jsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom";
 import Clock from "./clock";
 import Tabs from "./tabs";
 
+const panels = [
+  { title: "one", content: "I am the first" },
+  { title: "two", content: "I am the second" },
+  { title: "three", content: "I am the third" }
+];
+
+function Root(){
+    return(
+        <div>
+            <Clock/>
+            <Tabs panels={panels} />
+        </div>
+    )
+}
 
 document.addEventListener('DOMContentLoaded', ()=> {
     const root = document.getElementById('root')
-    ReactDOM.render(< Clock />, root)
-    ReactDOM.render(< Tabs />, root);
+    ReactDOM.render(< Root />, root)
 })
 
 
